019717dcb69f9843228797e34862e05341c1cbae
jatintomar04 <jatin.tomar.04@gmail.com>
2025-04-17 16:48:58 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// Import the right package for Tailwind with Vite
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer
-      ],
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://auto-fix-pro.onrender.com/",
-        changeOrigin: true,
-        secure: false,
+  plugins: [react(), tailwindcss()],
+  server : {
+    proxy : {
+      "/api" :{
+        target : "https://auto-fix-pro.onrender.com/",
+        changeOrigin : true,
+        secure :false,
       }
     }
   }
